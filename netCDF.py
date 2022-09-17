9eac67979b663a02bbc034722b5cb2b007b881cf
unknown <abdallaheldesouky385@gmail.com>
2022-09-17 03:13:23 +0200

    create csv file

--- netCDF.py
+++ netCDF.py
@@ -1,17 +1,29 @@
 from netCDF4 import Dataset
 import numpy as np
 import os
+import time
+import csv
+
+def getCSV(x, y, z, t):
+    '''this function create csv file with cartizian coorninates'''
+    with open('netCDF.csv','a') as file:
+        field_names = ['x', 'y', 'z', 't']
+        writer = csv.DictWriter(file,fieldnames=field_names)
+        writer.writeheader()
+        writer.writerow({'x':x,'y':y,'z':z,'t':t})
+
+
 
 
 def getCartizian(filename):
-    # angle in degree lists
-    radial_azims_degs = []
-    radial_elev_degs = []
-
+    """This function takes nc file and then converts polar coordinates to cartizian coordiantes and takes epoch time
+    then convert it to local time """
     # reading netCDF file
     data = Dataset(filename)
 
     # variables
+    radial_azims_degs = []
+    radial_elev_degs = []
     site_alt: object = data.variables['siteAlt'][:]  # Altitude of site above mean sea level
     site_lon = data.variables['siteLon'][:]  # Longitude of site
     site_lat = data.variables['siteLat'][:]  # Latitude of site
@@ -29,22 +41,33 @@ def getCartizian(filename):
         radial_elev_degs.append(radial_elev_deg)
 
     # calculate r and rho
-    r = np.sqrt(site_lon ** 2 + site_lat ** 2 + site_alt ** 2)
-    rho = np.sqrt(site_lon ** 2 + site_lat ** 2)
 
-    # convert to cartizan coordinates
-    for radial_elev_deg, radial_azims_degs in zip(radial_elev_degs, radial_azims_degs):
-        x = np.round(np.real(r * np.sin(radial_elev_deg)
-                             * np.cos(radial_azims_degs)), 3)
-        y = np.round(np.real(r * np.sin(radial_elev_deg)
-                             * np.sin(radial_azims_degs)), 3)
-        z = np.round(np.real((r * np.cos(radial_elev_deg))), 3)
-        print(f'({x}, {y}, {z})')
+    # r = np.sqrt(site_lon ** 2 + site_lat ** 2 + site_alt ** 2)
+    # rho = np.sqrt(site_lon ** 2 + site_lat ** 2)
+
+    for radial_elev_deg, radial_azims_deg, radial_time_epoch\
+    in zip(radial_elev_degs, radial_azims_degs, radial_time):
+        # calculate to cartizan coordinates
+        x = np.round(np.real(1 * np.sin(radial_elev_deg)
+                             * np.cos(radial_azims_deg)), 3)
+        y = np.round(np.real(1 * np.sin(radial_elev_deg)
+                             * np.sin(radial_azims_deg)), 3)
+        z = np.round(np.real((1 * np.cos(radial_elev_deg))), 3) # I think I need to add site_alt on the z value
+        # calculate local time
+        t = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(radial_time_epoch)))
+        getCSV(x, y, z, t)
+    print('cvs file created')
+        
+        # print(f'({x}, {y}, {z}, {t})')
 
 
+# loop through all nc files in directory where script exist
 for current_dir, dir_names, file_names in os.walk(os.getcwd()):
     for file_name in file_names:
-        firstpart, ext = os.path.splitext(file_names)
+        firstpart, ext = os.path.splitext(file_name)
         ext = ext.strip('.')
         if ext == 'nc':
             getCartizian(file_name)
+            
+            
+
